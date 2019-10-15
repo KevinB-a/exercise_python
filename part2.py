200a731a2bdcf3153c9ac80afb78b377b298469f
Kevin <kevin.billet.pro@gmail.com>
2019-10-15 16:50:39 +0200

    add exercise 1

--- part2.py
+++ part2.py
@@ -1,10 +1,5 @@
 # exercise1 true or false
 variable=""
 variable1="OK"
-def empty(a):
-    if a !="":
-        print("False")
-    else :
-        print("True")
-empty(variable)
-empty(variable1)
+print(bool(variable))
+print(bool(variable1))
