a112dd68e3b17ba20510a453a6d91f36152511a0
Kevin <kevin.billet.pro@gmail.com>
2019-10-15 15:40:58 +0200

    add exercise6

--- part1.py
+++ part1.py
@@ -19,8 +19,8 @@ print(12-7)
 print(5e4)
 
 #exercise3 : communicate with computer
-nom=input("veuillez entrez votre nom")
-print("bonjour et bienvenue !",nom)
+name=input("please enter your name")
+print("hello and welcome!",name)
 
 #exercise4 : first name and last name
 first_name="kevin"
@@ -31,3 +31,8 @@ print("Bonjour {} {}".format(first_name,last_name))
 myNumber="123"
 int_myNumber=int(myNumber) # create a new variable and converting to integer
 type(int_myNumber)
+
+#exercise6 : upper and lower case
+word=input("please enter a word")
+word.upper() #change lower letters in upper letters
+word.lower() #change upper letters in lower letters
